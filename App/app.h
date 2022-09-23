0939333133c2dfe7a811680d3fddf69dca598063
Inna Sorina <sig@vpribor.spb.ru>
2022-09-23 15:42:11 +0300

    Initial commit.

---
+++ App/app.h
@@ -0,0 +1,38 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define TRUE 1
+#define FALSE 0
+
+#define PASSED 0
+#define FAILED 1
+
+#define HARDWARE_TESTING_MODE
+
+typedef enum {
+    ENABLE_CMD = 0,
+    DISABLE_CMD,
+    NUM_PERIPH_COMMANDS
+} ePeriphCmd;
+
+typedef enum {
+    GPIO_PORT_A = 0,
+    GPIO_PORT_B,
+    GPIO_PORT_C,
+    GPIO_PORT_D,
+    GPIO_PORT_E,
+    GPIO_PORT_F,
+    GPIO_PORT_G,
+    GPIO_PORT_H,
+    GPIO_PORT_I,
+    USART_1,
+    USART_2,
+    USART_3,
+    UART_4,
+    UART_5,
+    USART_6,
+    DMA_1,
+    DMA_2
+} ePeriphTypes;
+
+#endif //_COMMON_H_
