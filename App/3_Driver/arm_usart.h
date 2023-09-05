e2b6e854525293e0a9a3699f90531b3f44a9101b
Inna Sorina <sig@vpribor.spb.ru>
2023-09-05 19:04:33 +0300

    Версия 1.0.11. RunTime конфигурирование UARTx перенесено в arm_usart.h

--- App/3_Driver/arm_usart.h
+++ App/3_Driver/arm_usart.h
@@ -5,6 +5,66 @@
 #include <stdbool.h>
 #include "ringbuffer.h"
 
+//select UARTx
+//use DMA if necessary
+//use DMA circular buffer if necessary (default - linear buffer)
+
+/*******************************************
+UART4
+
+For DMA:
+DMA2 CHANNEL5 (default config) - Tx
+DMA2_CHANNEL3 (default config) - Rx
+********************************************/
+#define _UART4_PERIPH_ENABLE_
+#define _UART4_TX_USE_DMA_
+#define _UART4_TX_DMA_CIRCULAR_MODE_
+#define _UART4_RX_USE_DMA_
+#define _UART4_RX_DMA_CIRCULAR_MODE_
+
+/*******************************************
+UART5
+
+For DMA:
+DMA1 CHANNEL4 (with flexible mapping) - Tx
+DMA1_CHANNEL5 (with flexible mapping) - Rx
+********************************************/
+#define _UART5_PERIPH_
+#define _UART5_TX_USE_DMA_
+#define _UART5_TX_DMA_CIRCULAR_MODE_
+#define _UART5_RX_USE_DMA_
+#define _UART5_RX_DMA_CIRCULAR_MODE_
+
+/*******************************************
+UART7
+
+For DMA:
+DMA1 CHANNEL2 (with flexible mapping) - Tx
+DMA1_CHANNEL3 (with flexible mapping) - Rx
+********************************************/
+#define _UART7_PERIPH_
+#define _UART7_TX_USE_DMA_
+#define _UART7_TX_DMA_CIRCULAR_MODE_
+#define _UART7_RX_USE_DMA_
+#define _UART7_RX_DMA_CIRCULAR_MODE_
+
+/*******************************************
+UART8
+
+For DMA:
+DMA2 CHANNEL6 (with flexible mapping) - Tx
+DMA2_CHANNEL4 (with flexible mapping) - Rx
+********************************************/
+#define _UART8_PERIPH_
+#define _UART8_TX_USE_DMA_
+#define _UART8_TX_DMA_CIRCULAR_MODE_
+#define _UART8_RX_USE_DMA_
+#define _UART8_RX_DMA_CIRCULAR_MODE_
+
+/*******************************************
+
+********************************************/
+
 #define ARM_USART_TX_BUFF_SIZE 256
 #define ARM_USART_RX_BUFF_SIZE 256
 
